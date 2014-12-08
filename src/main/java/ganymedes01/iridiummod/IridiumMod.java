a9e27ee7eb3795f59445e4158f729f91a1e81ef4
ganymedes01 <foka_12@hotmail.com>
2014-12-08 13:21:49 -0200

    Register IC2's iridium in the ore dictionary

--- src/main/java/ganymedes01/iridiummod/IridiumMod.java
+++ src/main/java/ganymedes01/iridiummod/IridiumMod.java
@@ -4,11 +4,13 @@ import ganymedes01.iridiummod.configuration.ConfigHandler;
 import ganymedes01.iridiummod.lib.Reference;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockOre;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -27,5 +29,19 @@ public class IridiumMod {
 
 		ConfigHandler.INSTANCE.preInit(event.getSuggestedConfigurationFile());
 		FMLCommonHandler.instance().bus().register(ConfigHandler.INSTANCE);
+	}
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		OreDictionary.registerOre("ingotIridium", getICItem("iridiumOre"));
+	}
+	
+	private static ItemStack getICItem(String name) {
+		try {
+			Class<?> itemsClass = Class.forName("ic2.core.Ic2Items");
+			return (ItemStack) itemsClass.getField(name).get(null);
+		} catch (Exception e) {
+			throw new IllegalArgumentException("IC2 item not found: " + name);
+		}
 	}
 }
