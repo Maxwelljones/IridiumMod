545f58dd09b508c34d9902a0425e16df24aa9054
ganymedes01 <foka_12@hotmail.com>
2014-12-08 23:26:06 -0200

    Added iridium ingot instead of registering IC2's 🚀🚀

--- src/main/java/ganymedes01/iridiummod/IridiumMod.java
+++ src/main/java/ganymedes01/iridiummod/IridiumMod.java
@@ -4,13 +4,13 @@ import ganymedes01.iridiummod.configuration.ConfigHandler;
 import ganymedes01.iridiummod.lib.Reference;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockOre;
-import net.minecraft.item.ItemStack;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
 import net.minecraftforge.oredict.OreDictionary;
 import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.Mod.Instance;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -21,27 +21,17 @@ public class IridiumMod {
 	public static IridiumMod instance;
 
 	public static final Block iridiumOre = new BlockOre().setStepSound(Block.soundTypePiston).setBlockName(Reference.MOD_ID + ".oreIridium").setBlockTextureName(Reference.MOD_ID + ":iridium_ore");
+	public static final Item iridiumIngot = new Item().setUnlocalizedName(Reference.MOD_ID + ".ingotIridium").setCreativeTab(CreativeTabs.tabMaterials).setTextureName(Reference.MOD_ID + ":iridium_ingot");
 
 	@EventHandler
 	public void preInit(FMLPreInitializationEvent event) {
 		GameRegistry.registerBlock(iridiumOre, "iridiumOre");
 		OreDictionary.registerOre("oreIridium", iridiumOre);
 
+		GameRegistry.registerItem(iridiumIngot, "iridiumIngot");
+		OreDictionary.registerOre("ingotIridium", iridiumIngot);
+
 		ConfigHandler.INSTANCE.preInit(event.getSuggestedConfigurationFile());
 		FMLCommonHandler.instance().bus().register(ConfigHandler.INSTANCE);
-	}
-	
-	@EventHandler
-	public void init(FMLInitializationEvent event) {
-		OreDictionary.registerOre("ingotIridium", getICItem("iridiumOre"));
-	}
-	
-	private static ItemStack getICItem(String name) {
-		try {
-			Class<?> itemsClass = Class.forName("ic2.core.Ic2Items");
-			return (ItemStack) itemsClass.getField(name).get(null);
-		} catch (Exception e) {
-			throw new IllegalArgumentException("IC2 item not found: " + name);
-		}
 	}
 }
--- src/main/java/ganymedes01/iridiummod/lib/Reference.java
+++ src/main/java/ganymedes01/iridiummod/lib/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MOD_ID = "iridiummod";
 	public static final String MOD_NAME = "Iridium Mod";
-	public static final String DEPENDENCIES = "required-after:Forge@[10.13.0.1180,);after:*";
+	public static final String DEPENDENCIES = "required-after:Forge@[10.13.0.1180,);";
 	public static final String VERSION_NUMBER = "1.0.1";
 
 	public static final String GUI_FACTORY_CLASS = "ganymedes01.iridiummod.configuration.ConfigGuiFactory";
